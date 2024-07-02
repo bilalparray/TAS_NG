da068ff5d7b49467d9bda2cf18e51614314c4c4c
Bilal Ahmad <dev.renosoftwares@gmail.com>
2024-07-02 13:48:15 +0530

    add icons more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+// import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { MomComponent } from './components/mom/mom.component';
+// import { MomComponent } from './components/mom/mom.component';
 import { HomeComponent } from './components/home/home.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,16 +13,18 @@ import { PlayerComponent } from './components/player/player.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BattingOrderComponent } from './components/batting-order/batting-order.component';
 import { NgxUiLoaderModule } from 'ngx-ui-loader';
+import { BottomNavbarComponent } from './components/bottom-navbar/bottom-navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+    // NavbarComponent,
     FooterComponent,
-    MomComponent,
+    // MomComponent,
     HomeComponent,
     CardsComponent,
     PlayerComponent,
     BattingOrderComponent,
+    BottomNavbarComponent,
   ],
   imports: [
     BrowserModule,
