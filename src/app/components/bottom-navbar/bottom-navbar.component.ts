cf6db7627be16a9d07b255d4a3737b44240b73c6
bilalparray <parraybilal34@gmail.com>
2024-07-17 12:12:21 +0530

    side bar

--- src/app/components/bottom-navbar/bottom-navbar.component.ts
+++ src/app/components/bottom-navbar/bottom-navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-bottom-navbar',
   templateUrl: './bottom-navbar.component.html',
-  styleUrls: ['./bottom-navbar.component.scss']
+  styleUrls: ['./bottom-navbar.component.scss'],
 })
 export class BottomNavbarComponent {
-
+  version: string = '';
+  ngOnInit() {
+    this.version = environment.applicationVersion;
+  }
 }
