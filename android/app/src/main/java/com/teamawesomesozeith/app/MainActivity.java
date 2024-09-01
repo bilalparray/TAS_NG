7fcd51e3b6a58ebdb80254e4a1183211b0f59a31
bilalparray <parraybilal34@gmail.com>
2024-09-01 10:44:04 +0530

    test ads integrated

---
+++ android/app/src/main/java/com/teamawesomesozeith/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.teamawesomesozeith.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,13 @@ import { Toast } from '@capacitor/toast';
 import { AppConstants } from 'src/app-constants';
 import { environment } from 'src/environments/environment';
 import { AppVersionSM } from './models/service/v1/app-version-sm';
+import {
+  AdMob,
+  AdMobInitializationOptions,
+  BannerAdOptions,
+  BannerAdPosition,
+  BannerAdSize,
+} from '@capacitor-community/admob';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -30,6 +37,7 @@ export class AppComponent {
   }
   async initializeApp() {
     this.setupBackButtonListener();
+    await this.initializeAds();
     this.getAllVersionInfoAndCheckForUpdates();
     App.addListener('appStateChange', ({ isActive }) => {
       if (isActive) {
@@ -111,5 +119,25 @@ export class AppComponent {
       });
       throw new Error(AppConstants.ErrorPrompts.Unknown_Error);
     }
+  }
+
+  async initializeAds() {
+    if (environment.controlAds.showAds == false) return;
+    try {
+      let options: AdMobInitializationOptions = { initializeForTesting: false };
+      await AdMob.initialize(options);
+    } catch (error) {}
+
+    if (environment.controlAds.showAdMobBannerAds) await this.ShowBannerAds();
+  }
+
+  async ShowBannerAds() {
+    const options: BannerAdOptions = {
+      adId: environment.controlAds.admobBannerAdUnitId,
+      adSize: BannerAdSize.ADAPTIVE_BANNER,
+      position: BannerAdPosition.BOTTOM_CENTER,
+      isTesting: !environment.production,
+    };
+    AdMob.showBanner(options);
   }
 }
